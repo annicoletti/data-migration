6e27e6f50df6b8eebb243342dfa67c21d8b5fd36
quarkusio <no-reply@quarkus.io>
2021-03-27 03:08:18 +0000

    Initial commit

---
+++ src/test/java/br/com/cpqd/avm/etl/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.cpqd.avm.etl;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
